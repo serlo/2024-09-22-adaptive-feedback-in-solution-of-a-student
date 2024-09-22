319a0e192a71511e8e831b2314fafc4161535988
Stephan Kulla <git.mail@kulla.me>
2024-09-22 17:23:29 +0200

    wip: Add basic structure

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,16 @@ import { Heading } from '@radix-ui/themes'
 
 export default function Home() {
   return (
-    <main>
-      <Heading>Hello World</Heading>
+    <main className="mx-auto max-w-[720px] p-4">
+      <Heading>TODO: Aufgabenstellung hier rendern</Heading>
+      <div className="rounded border p-4 my-4">
+        <div className="bg-orange-100">TODO: Toolbar</div>
+        <div className="flex justify-end mt-2 space-x-2">
+          <button className="button">Hilfe</button>
+          <button className="button">Abschicken</button>
+        </div>
+      </div>
+      <Heading>TODO: Musterlösung hier rendern</Heading>
     </main>
   )
 }
