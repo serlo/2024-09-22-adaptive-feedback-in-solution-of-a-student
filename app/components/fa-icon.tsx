af4b11995bd49b6e2b42993873fabca99ff4f811
Botho <1258870+elbotho@users.noreply.github.com>
2024-09-23 10:20:09 +0200

    add serlo fa icon component

---
+++ app/components/fa-icon.tsx
@@ -0,0 +1,28 @@
+import type { IconDefinition } from '@fortawesome/fontawesome-svg-core'
+import { CSSProperties } from 'react'
+import { cn } from '../helper/cn'
+
+export interface FaIconProps {
+  icon: IconDefinition
+  className?: string
+  style?: CSSProperties
+}
+
+export function FaIcon({ icon, className, style }: FaIconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      viewBox={`0 0 ${icon.icon[0]} ${icon.icon[1]}`}
+      aria-hidden
+      focusable={false}
+      className={cn('serlo-fa-icon', className)}
+      style={style}
+    >
+      <path
+        fill="currentColor"
+        d={Array.isArray(icon.icon[4]) ? icon.icon[4][0] : icon.icon[4]}
+      ></path>
+    </svg>
+  )
+}
